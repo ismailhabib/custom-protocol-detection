ccbcb7a35a64aed9627b702c814a92eb40f74c78
Ismail Habib Muhammad <ismail.muhammad@mendix.com>
2015-12-25 07:05:12 +0100

    Add support for Edge and Windows 10 by checking navigator.msLaunchUri first.

--- example.js
+++ example.js
@@ -2,9 +2,8 @@ $(function () {
     $("div[href]").click(function (event) {
         window.protocolCheck($(this).attr("href"),
             function () {
-                alert("fail");
+                alert("protocol not recognized");
             });
         event.preventDefault ? event.preventDefault() : event.returnValue = false;
     });
-})
-;
+});
