e19bf1c71b582b0c862926f386939c87020a4c30
Ryan Heath <ryan.heath@software2inc.com>
2016-08-08 11:37:32 +0100

    Added a success callback to all functions which is called after the protocol has been launched

--- protocolcheck.js
+++ protocolcheck.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function(window) {
 
     function _registerEvent(target, eventType, cb) {
         if (target.addEventListener) {
@@ -24,10 +24,11 @@
         iframe.id = "hiddenIframe";
         iframe.style.display = "none";
         target.appendChild(iframe);
+
         return iframe;
     }
 
-    function openUriWithHiddenFrame(uri, failCb) {
+    function openUriWithHiddenFrame(uri, failCb, successCb) {
 
         var timeout = setTimeout(function () {
             failCb();
@@ -44,13 +45,14 @@
         function onBlur() {
             clearTimeout(timeout);
             handler.remove();
+            successCb();
         }
 
         iframe.contentWindow.location.href = uri;
     }
 
-    function openUriWithTimeoutHack(uri, failCb) {
-
+    function openUriWithTimeoutHack(uri, failCb, successCb) {
+        
         var timeout = setTimeout(function () {
             failCb();
             handler.remove();
@@ -67,18 +69,22 @@
         function onBlur() {
             clearTimeout(timeout);
             handler.remove();
+            successCb();
         }
 
         window.location = uri;
     }
 
-    function openUriUsingFirefox(uri, failCb) {
+    function openUriUsingFirefox(uri, failCb, successCb) {
         var iframe = document.querySelector("#hiddenIframe");
+
         if (!iframe) {
             iframe = _createHiddenIframe(document.body, "about:blank");
         }
+
         try {
             iframe.contentWindow.location.href = uri;
+            successCb();
         } catch (e) {
             if (e.name == "NS_ERROR_UNKNOWN_PROTOCOL") {
                 failCb();
@@ -86,20 +92,21 @@
         }
     }
 
-    function openUriUsingIEInOlderWindows(uri, failCb) {
+    function openUriUsingIEInOlderWindows(uri, failCb, successCb) {
         if (getInternetExplorerVersion() === 10) {
-            openUriUsingIE10InWindows7(uri, failCb);
+            openUriUsingIE10InWindows7(uri, failCb, successCb);
         } else if (getInternetExplorerVersion() === 9 || getInternetExplorerVersion() === 11) {
-            openUriWithHiddenFrame(uri, failCb);
+            openUriWithHiddenFrame(uri, failCb, successCb);
         } else {
-            openUriInNewWindowHack(uri, failCb);
+            openUriInNewWindowHack(uri, failCb, successCb);
         }
     }
 
-    function openUriUsingIE10InWindows7(uri, failCb) {
+    function openUriUsingIE10InWindows7(uri, failCb, successCb) {
         var timeout = setTimeout(failCb, 1000);
         window.addEventListener("blur", function () {
             clearTimeout(timeout);
+            successCb();
         });
 
         var iframe = document.querySelector("#hiddenIframe");
@@ -114,37 +121,38 @@
         }
     }
 
-    function openUriInNewWindowHack(uri, failCb) {
+    function openUriInNewWindowHack(uri, failCb, successCb) {
         var myWindow = window.open('', '', 'width=0,height=0');
 
         myWindow.document.write("<iframe src='" + uri + "'></iframe>");
+
         setTimeout(function () {
             try {
                 myWindow.location.href;
                 myWindow.setTimeout("window.close()", 1000);
+                successCb();
             } catch (e) {
                 myWindow.close();
                 failCb();
             }
         }, 1000);
     }
-
+    
     function openUriWithMsLaunchUri(uri, failCb) {
         navigator.msLaunchUri(uri,
-            function () {
-            },
+            successCb,
             failCb
-            );
+        );
     }
-
+    
     function checkBrowser() {
         var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
         return {
-            isOpera: isOpera,
-            isFirefox: typeof InstallTrigger !== 'undefined',
-            isSafari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
-            isChrome: !!window.chrome && !isOpera,
-            isIE: /*@cc_on!@*/false || !!document.documentMode   // At least IE6
+            isOpera   : isOpera,
+            isFirefox : typeof InstallTrigger !== 'undefined',
+            isSafari  : Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
+            isChrome  : !!window.chrome && !isOpera,
+            isIE      : /*@cc_on!@*/false || !!document.documentMode // At least IE6
         }
     }
 
@@ -166,22 +174,26 @@
         return rv;
     }
 
-    window.protocolCheck = function (uri, failCb) {
+    window.protocolCheck = function(uri, failCb, successCb) {
         function failCallback() {
             failCb && failCb();
         }
 
+        function successCallback() {
+            successCb && successCb();
+        }
+
         if (navigator.msLaunchUri) { //for IE and Edge in Win 8 and Win 10
-            openUriWithMsLaunchUri(uri, failCb);
+            openUriWithMsLaunchUri(url, failCb, successCb);
         } else {
             var browser = checkBrowser();
 
             if (browser.isFirefox) {
-                openUriUsingFirefox(uri, failCallback);
+                openUriUsingFirefox(uri, failCallback, successCallback);
             } else if (browser.isChrome) {
-                openUriWithTimeoutHack(uri, failCallback);
+                openUriWithTimeoutHack(uri, failCallback, successCallback);
             } else if (browser.isIE) {
-                openUriUsingIEInOlderWindows(uri, failCallback);
+                openUriUsingIEInOlderWindows(uri, failCallback, successCallback);
             } else {
                 //not supported, implement please
             }
