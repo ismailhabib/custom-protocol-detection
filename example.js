441fe4ac49d814953e847b7e4ab8046ee9657801
Emil Atanasov <heitara@gmail.com>
2020-09-15 14:11:29 +0300

    fix the FF behavior. The iFrame should be recreated each time

--- example.js
+++ example.js
@@ -1,9 +1,15 @@
 $(function () {
     $("div[href]").click(function (event) {
-        window.protocolCheck($(this).attr("href"),
+        let link = $(this).attr("href")
+        console.log("Link = ", link)
+        window.protocolCheck(link,
             function () {
-                alert("protocol not recognized");
-            });
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+               console.log("unrecognized")
+            }, function success() {
+                console.log("success")
+             },  function unSupported() {
+                console.log("unSupported")
+             })
+        event.preventDefault ? event.preventDefault() : event.returnValue = false
     });
 });
